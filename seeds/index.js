6c57babd0963a6a7a8e779942144fc6dbec51a7d
Kevin Reznicek <kreznicek@me.com>
2021-08-23 18:03:20 -0400

    Add seed data generation script

---
+++ seeds/index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { descriptors, places } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+mongoose
+  .connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Mongo database connected.");
+  })
+  .catch((err) => {
+    console.log("Mongo connection error:");
+    console.log(err);
+  });
+
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const c = new Campground({
+      title: `${sample(descriptors)} ${sample(places)}`,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+    });
+    await c.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+  console.log("Mongo database disconnected.");
+});
