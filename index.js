b778bed6e20c4b2677efd6422179f9aa1d6c2551
Kevin Reznicek <kreznicek@me.com>
2021-08-25 00:09:26 -0400

    Add ejs-mate to index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 const methodOverride = require("method-override");
 
@@ -22,6 +23,7 @@ mongoose
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
