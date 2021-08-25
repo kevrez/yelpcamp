db5da35efe933dbae7e9702a2eabca5f5920f656
Kevin Reznicek <kreznicek@me.com>
2021-08-25 00:07:58 -0400

    Rename index.js to reSeed.js for clarity

--- seeds/reSeed.js
+++ seeds/reSeed.js
@@ -25,9 +25,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const c = new Campground({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quam ducimus nobis consequuntur, tempore, pariatur iusto sit qui, omnis eligendi corporis praesentium! Non maiores laborum magni inventore nihil obcaecati earum!",
+      price,
     });
     await c.save();
   }
