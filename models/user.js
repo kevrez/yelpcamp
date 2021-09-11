620da1b0171def0dbea221a147757e0071ac8aea
Kevin Reznicek <kreznicek@me.com>
2021-09-10 23:13:57 -0400

    Create user mongoose schema

---
+++ models/user.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+UserSchema.plugin(passportLocalMongoose);
+
+UserSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: { $in: doc.reviews },
+    });
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
