a56517233d86b2198b3343cb46ff941c4952e527
Kevin Reznicek <kreznicek@me.com>
2021-09-12 22:06:03 -0400

    Refactor - move middleware functions into middleware.js

--- middleware.js
+++ middleware.js
@@ -1,10 +1,64 @@
 "use strict";
 
+const { campgroundSchema, reviewSchema } = require("./schemas/schemas.js");
+const ExpressError = require("./utils/ExpressError");
+const Campground = require("./models/campground");
+const Review = require("./models/review");
+
 module.exports.isLoggedIn = (req, res, next) => {
-  req.session.returnTo = req.originalUrl;
   if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
     req.flash("error", "You must be signed in to do that.");
     res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports.validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((elem) => elem.message).join(", ");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    req.flash("error", "Can't find that campground.");
+    res.redirect("/campgrounds");
+  } else if (!campground.author.equals(req.user._id)) {
+    req.flash("error", "You don't have permission to do that.");
+    res.redirect(`/campgrounds/${id}`);
+  } else {
+    next();
+  }
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review) {
+    req.flash("error", "Can't find that review.");
+    res.redirect(`/campgrounds/${id}`);
+  } else if (!review.author.equals(req.user._id)) {
+    req.flash("error", "You don't have permission to do that.");
+    res.redirect(`/campgrounds/${id}`);
+  } else {
+    next();
+  }
+};
+
+module.exports.validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    console.log(error);
+    const msg = error.details.map((elem) => elem.message).join(", ");
+    throw new ExpressError(msg, 400);
   } else {
     next();
   }
