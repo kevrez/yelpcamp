e7548e9126938ed81cb55cbb7d33ccc0a453b22c
Kevin Reznicek <kreznicek@me.com>
2021-09-16 22:55:05 -0400

    Add virtual property to images for thumbnail get links

--- models/campground.js
+++ models/campground.js
@@ -5,15 +5,20 @@ const Review = require("./review");
 const Schema = mongoose.Schema;
 const User = require("./user");
 
+// https://res.cloudinary.com/dqihgrxvp/image/upload/v1631834245/YelpCamp/bajo903fjbcfk1rwqrej.jpg'
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/c_fill,w_125,h_125");
+});
+
 const CampgroundSchema = new Schema({
   title: String,
   price: Number,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   description: String,
   location: String,
   author: {
