8df2b2fe74d23f9f29d3130b266cc0340e618156
Kevin Reznicek <kreznicek@me.com>
2021-09-17 17:16:30 -0400

    Add MapBox map to show page

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log(campground);
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/light-v10",
+  center: campground.geometry.coordinates,
+  zoom: 10,
+});
+
+const marker = new mapboxgl.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({
+      offset: 25,
+    }).setHTML(`<h4>${campground.title}</h4><p>${campground.location}</p>`)
+  )
+  .addTo(map);
