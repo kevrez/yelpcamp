a48e5592c5044d0102ecf8d70107819510692224
Kevin Reznicek <kreznicek@me.com>
2021-09-20 17:55:12 -0400

    Add map with clustering to Campgrounds page

--- models/campground.js
+++ models/campground.js
@@ -15,34 +15,43 @@ ImageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "/upload/c_fill,w_125,h_125");
 });
 
-const CampgroundSchema = new Schema({
-  title: String,
-  price: Number,
-  images: [ImageSchema],
-  geometry: {
-    type: {
-      type: String,
-      enum: ["Point"],
-      required: true,
-    },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
+const opts = { toJSON: { virtuals: true } };
 
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: User,
-  },
-  reviews: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Review",
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    price: Number,
+    images: [ImageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-  ],
+
+    description: String,
+    location: String,
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: User,
+    },
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
+
+CampgroundSchema.virtual("properties.popupMarkup").get(function () {
+  return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong><p>${this.location}</p>`;
 });
 
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
