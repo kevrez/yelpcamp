948009523cf777b32b4716c0a31ec4faabd5b123
Kevin Reznicek <kreznicek@me.com>
2021-09-23 22:42:19 -0400

    Refactor, reorganize & add comments

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 "use strict";
 
+// Environment variable imports
+const dbURL = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+const secret = process.env.SECRET || "thisshouldbeabettersecret";
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
-const express = require("express");
-const ExpressError = require("./utils/ExpressError");
-const methodOverride = require("method-override");
+// Local machine imports
 const path = require("path");
-const mongoose = require("mongoose");
-const ejsMate = require("ejs-mate");
+// Express imports
+const express = require("express");
+const methodOverride = require("method-override");
 const session = require("express-session");
-const flash = require("connect-flash");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const User = require("./models/user");
+const ExpressError = require("./utils/ExpressError");
+// Database imports
+const mongoose = require("mongoose");
 const mongoSanitize = require("express-mongo-sanitize");
-
+const MongoStore = require("connect-mongo");
+const User = require("./models/user");
+// Page rendering imports
 const campgroundRoutes = require("./routes/campgroundRoutes");
 const reviewRoutes = require("./routes/reviewRoutes");
 const userRoutes = require("./routes/userRoutes");
+const ejsMate = require("ejs-mate");
+const flash = require("connect-flash");
+// Session imports
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+// Security imports
 const helmet = require("helmet");
-const MongoStore = require("connect-mongo");
-
-const dbURL = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
-const secret = process.env.SECRET || "thisshouldbeabettersecret";
+const CSP = require("./security/contentSecurityPolicy");
 
 const app = express();
 
+// Express setup
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Database setup
 mongoose
   .connect(dbURL, {
     useNewUrlParser: true,
@@ -43,15 +54,10 @@ mongoose
     console.log(err);
   });
 
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
+// Mongo injection sanitizer
 app.use(mongoSanitize());
 
+// Mongo store for session
 const store = MongoStore.create({
   mongoUrl: dbURL,
   secret,
@@ -61,6 +67,20 @@ const store = MongoStore.create({
 store.on("error", function (e) {
   console.log("Session store error:", e);
 });
+
+// Page rendering setup
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(flash());
+
+// Session setup
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const sessionConfig = {
   store,
@@ -76,77 +96,25 @@ const sessionConfig = {
   },
 };
 app.use(session(sessionConfig));
-app.use(flash());
+
+// Security setup
 app.use(helmet({ contentSecurityPolicy: false }));
+app.use(helmet.contentSecurityPolicy(CSP));
 
-const scriptSrcUrls = [
-  "https://stackpath.bootstrapcdn.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://api.mapbox.com/",
-  "https://kit.fontawesome.com/",
-  "https://cdnjs.cloudflare.com/",
-  "https://cdn.jsdelivr.net",
-  "https://intense-springs-95269.herokuapp.com",
-];
-const styleSrcUrls = [
-  "https://kit-free.fontawesome.com/",
-  "https://stackpath.bootstrapcdn.com/",
-  "https://api.mapbox.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://fonts.googleapis.com/",
-  "https://use.fontawesome.com/",
-  "https://cdn.jsdelivr.net",
-];
-const connectSrcUrls = [
-  "https://api.mapbox.com/",
-  "https://a.tiles.mapbox.com/",
-  "https://b.tiles.mapbox.com/",
-  "https://events.mapbox.com/",
-  "https://*.tiles.mapbox.com",
-  "https://events.mapbox.com",
-];
-const fontSrcUrls = [];
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: [],
-      connectSrc: ["'self'", ...connectSrcUrls],
-      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-      workerSrc: ["'self'", "blob:"],
-      objectSrc: [],
-      imgSrc: [
-        "'self'",
-        "blob:",
-        "data:",
-        `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`, //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
-        "https://images.unsplash.com/",
-      ],
-      fontSrc: ["'self'", ...fontSrcUrls],
-      childSrc: ["'self'", "blob:"],
-    },
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
+// Flash messages middleware setup
 app.use((req, res, next) => {
-  // console.log(req.session);
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
+// Route controllers
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
 app.use("/", userRoutes);
 
+//General routes
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
@@ -155,6 +123,7 @@ app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
 });
 
+// Default error handling page
 app.use((err, req, res, next) => {
   if (!err.message) {
     err.message = "Something went wrong!";
@@ -162,9 +131,10 @@ app.use((err, req, res, next) => {
   if (!err.statusCode) err.statusCode = 500;
   res.status(err.statusCode).render("error.ejs", { err });
 });
+// TODO: Add 404 page
 
+// Express listener
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
   console.log(`Serving on port ${port}`);
 });
